12e6057b9ee40e7b587e86d8e29709207b3a8913
mlandry <mike.landry@gmail.com>
2022-12-04 18:23:04 +0000

    skeleton + input for day 4

---
+++ src/main/java/aoc2022/day04/CampCleanup.java
@@ -0,0 +1,9 @@
+package aoc2022.day04;
+
+/** Solution for https://adventofcode.com/2022/day/4. */class CampCleanup {
+
+  private static final String INPUT = "aoc2022/day04/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

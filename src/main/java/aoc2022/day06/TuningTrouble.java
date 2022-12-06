82d2bc32729ab44726568076798f575e5288caf5
mlandry <mike.landry@gmail.com>
2022-12-06 14:06:38 +0000

    Day 6 part 1 complete

--- src/main/java/aoc2022/day06/TuningTrouble.java
+++ src/main/java/aoc2022/day06/TuningTrouble.java
@@ -1,10 +1,36 @@
 package aoc2022.day06;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+import aoccommon.InputHelper;
+
 /** Solution for {@link https://adventofcode.com/2022/day/6}. */
 public class TuningTrouble {
 
   private static final String INPUT = "aoc2022/day06/input.txt";
 
   public static void main(String [] args) throws Exception {
+    String datastream = InputHelper.linesFromResource(INPUT).findFirst().get();
+    LinkedList<Character> buffer = new LinkedList<>();
+
+    int i = 0;
+    for (; i < datastream.length(); i++) {
+      char c = datastream.charAt(i);
+      if (buffer.size() == 4) {
+        buffer.removeFirst();
+      }
+      buffer.addLast(c);
+      if (buffer.size() == 4 && allDistinct(buffer)) {
+        break;
+      }
+    }
+    int marker = i + 1;
+    System.out.println("Part 1: " + marker);
+  }
+
+  private static boolean allDistinct(List<Character> buffer) {
+    return new HashSet<>(buffer).size() == buffer.size();
   }
 }

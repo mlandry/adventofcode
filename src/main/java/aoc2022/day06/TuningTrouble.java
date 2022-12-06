18a3ee66e2612dabd20bff52f6c7dfc5a286c251
mlandry <mike.landry@gmail.com>
2022-12-06 13:57:56 +0000

    Day 6 skeleton + input

---
+++ src/main/java/aoc2022/day06/TuningTrouble.java
@@ -0,0 +1,10 @@
+package aoc2022.day06;
+
+/** Solution for {@link https://adventofcode.com/2022/day/6}. */
+public class TuningTrouble {
+
+  private static final String INPUT = "aoc2022/day06/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

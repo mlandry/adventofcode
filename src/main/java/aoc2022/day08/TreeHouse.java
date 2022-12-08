1a08ea6922c6c49194c52e312f14e8f0615e9944
mlandry <mike.landry@gmail.com>
2022-12-08 15:52:30 +0000

    day 8 setup

---
+++ src/main/java/aoc2022/day08/TreeHouse.java
@@ -0,0 +1,10 @@
+package aoc2022.day08;
+
+/** Solution for {@link https://adventofcode.com/2022/day/8}. */
+public class TreeHouse {
+
+  private static final String INPUT = "aoc2022/day08/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

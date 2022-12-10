69110dda00ac0eb5cd00ddf324a278fcb3283188
mlandry <mike.landry@gmail.com>
2022-12-10 16:06:53 +0000

    Day 10 skeleton

---
+++ src/main/java/aoc2022/day10/CathodeRayTube.java
@@ -0,0 +1,10 @@
+package aoc2022.day10;
+
+/** Solution for {@link https://adventofcode.com/2022/day/10}. */
+public class CathodeRayTube {
+
+  private static final String INPUT = "aoc2022/day10/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

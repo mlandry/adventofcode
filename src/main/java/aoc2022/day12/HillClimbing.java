1eb0624766597b6209dfd954d7b0cc2769620e1b
mlandry <mike.landry@gmail.com>
2022-12-12 14:42:31 +0000

    Day 12 setup

---
+++ src/main/java/aoc2022/day12/HillClimbing.java
@@ -0,0 +1,10 @@
+package aoc2022.day12;
+
+/** Solution for {@link https://adventofcode.com/2022/day/12}. */
+public class HillClimbing {
+
+  private static final String INPUT = "aoc2022/day12/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

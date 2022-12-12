c204145260c173da31f628a8adcf31ed2347de87
mlandry <mike.landry@gmail.com>
2022-12-12 18:09:32 +0000

    2015 Day 1 skeleton

---
+++ src/main/java/aoc2015/day01/ApartmentBuilding.java
@@ -0,0 +1,10 @@
+package aoc2015.day01;
+
+/** Solution for {@link https://adventofcode.com/2015/day/1}. */
+public class ApartmentBuilding {
+
+  private static final String INPUT = "aoc2015/day01/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

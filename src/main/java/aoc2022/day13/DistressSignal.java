72ff4e824133bd1f501a7010c7dbc4389a7ccae1
mlandry <mike.landry@gmail.com>
2022-12-13 21:31:48 +0000

    Day 13 part 1

--- src/main/java/aoc2022/day13/DistressSignal.java
+++ src/main/java/aoc2022/day13/DistressSignal.java
@@ -8,6 +8,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.OptionalInt;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import aoccommon.InputHelper;
 
@@ -16,19 +17,34 @@ public class DistressSignal {
 
   private static final String INPUT = "aoc2022/day13/input.txt";
 
+  private static final boolean DEBUG = false;
+
+  private static void debug(String fmt, Object... args) {
+    if (DEBUG) {
+      System.out.println(String.format(fmt, args));
+    }
+  }
+
   public static void main(String[] args) throws Exception {
-    List<PacketPair> pairs = new ArrayList<>();
+    final List<PacketPair> pairs = new ArrayList<>();
 
     Iterator<String> iter = InputHelper.linesFromResource(INPUT).iterator();
     while (iter.hasNext()) {
       pairs.add(new PacketPair(parsePacket(iter.next()), parsePacket(iter.next())));
-      iter.next();
+      if (iter.hasNext()) {
+        iter.next();
+      }
     }
+
+    int indexSum = IntStream.range(0, pairs.size())
+        .map(i -> pairs.get(i).compare() < 0 ? i + 1 : 0)
+        .sum();
+    System.out.println("Part 1: " + indexSum);
   }
 
   private static Packet parsePacket(String line) {
     Deque<List<PacketData>> stack = new ArrayDeque<>();
-    stack.add(new ArrayList<>());
+    stack.push(new ArrayList<>());
 
     int i = 0;
     char c = line.charAt(i++);
@@ -36,24 +52,37 @@ public class DistressSignal {
       throw new IllegalStateException();
     }
 
-    while(i < line.length() - 1) {
+    while(i < line.length() - 1 /* exclude closing ] */) {
       c = line.charAt(i);
+      debug("Parsing char %s at index %d, stack=%s", c, i, stack);
       if (c == '[') {
-        stack.add(new ArrayList<>());
+        stack.push(new ArrayList<>());
         i++;
       } else if (c == ']') {
         List<PacketData> list = stack.pop();
+        // stack.peek().add(list.size() == 1 ? list.get(0) : PacketData.list(list));
         stack.peek().add(PacketData.list(list));
         i++;
       } else if (Character.isDigit(c)) {
-        
+        int value = 0;
+        while (Character.isDigit(c)) {
+          value *= 10;
+          value += (c - '0');
+          c = line.charAt(++i);
+        }
+        stack.peek().add(PacketData.value(value));
+      } else if (c == ',') {
+        i++;
+      } else {
+        throw new IllegalStateException();
       }
     }
-
-    return new Packet(stack.pop());
+    Packet packet = new Packet(stack.pop());
+    debug("Parsed: " + packet);
+    return packet;
   }
 
-  private static record PacketData(OptionalInt value, List<PacketData> list) {
+  private static record PacketData(OptionalInt value, List<PacketData> list) implements Comparable<PacketData> {
     private static PacketData value(int value) {
       return new PacketData(OptionalInt.of(value), new ArrayList<>());
     }
@@ -61,11 +90,67 @@ public class DistressSignal {
     private static PacketData list(List<PacketData> list) {
       return new PacketData(OptionalInt.empty(), list);
     }
+
+    private List<PacketData> asList() {
+      return value.isEmpty() ? list : List.of(this);
+    }
+
+    @Override
+    public int compareTo(PacketData other) {
+      if (value.isPresent() && other.value.isPresent()) {
+        return Integer.compare(value.getAsInt(), other.value.getAsInt());
+      }
+      return compareLists(asList(), other.asList());
+    }
+
+    @Override
+    public String toString() {
+      if (value.isPresent()) {
+        return Integer.toString(value.getAsInt());
+      }
+      return "[" + list.stream().map(PacketData::toString).collect(Collectors.joining(",")) + "]";
+    }
   }
 
-  private static record Packet(List<PacketData> data) {
+  private static record Packet(List<PacketData> data) implements Comparable<Packet> {
+    @Override
+    public int compareTo(Packet other) {
+      return compareLists(data, other.data);
+    }
+
+    @Override
+    public String toString() {
+      return "[" + data.stream().map(PacketData::toString).collect(Collectors.joining(",")) + "]";
+    }
   }
 
   private static record PacketPair(Packet left, Packet right) {
+    private int compare() {
+      debug("- Compare %s vs %s", left, right);
+      return left.compareTo(right);
+    }
+  }
+
+  private static int compareLists(List<PacketData> leftList, List<PacketData> rightList) {
+    Iterator<PacketData> leftIter = leftList.iterator();
+    Iterator<PacketData> rightIter = rightList.iterator();
+    while (leftIter.hasNext() || rightIter.hasNext()) {
+      if (!leftIter.hasNext()) {
+        debug("  - Left side ran out of items, so inputs are in the right order");
+        return -1;
+      } else if (!rightIter.hasNext()) {
+        debug("  - Right side ran out of items, so inputs are not in the right order");
+        return 1;
+      }
+      PacketData left = leftIter.next();
+      PacketData right = rightIter.next();
+      debug("  - Compare %s vs %s", left, right);
+      int compare = left.compareTo(right);
+      if (compare != 0) {
+        debug("    - %s side is smaller, so inputs are %sin the right order", compare < 0 ? "Left" : "Right", compare < 0 ? "" : "not ");
+        return compare;
+      }
+    }
+    return 0;
   }
 }

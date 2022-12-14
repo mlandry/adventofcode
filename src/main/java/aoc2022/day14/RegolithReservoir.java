879cde921a51d0fe9e5680dea03d795f37f86c90
mlandry <mike.landry@gmail.com>
2022-12-14 14:58:47 +0000

    Day 14 setup + input

---
+++ src/main/java/aoc2022/day14/RegolithReservoir.java
@@ -0,0 +1,10 @@
+package aoc2022.day14;
+
+/** Solution for {@link https://adventofcode.com/2022/day/14}. */
+public class RegolithReservoir {
+
+  private static final String INPUT = "aoc2022/day14/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

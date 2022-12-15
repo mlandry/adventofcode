6b8a921fb5ee80bace8f1db3866d3cbaac1c0c66
mlandry <mike.landry@gmail.com>
2022-12-15 16:38:27 +0000

    Day 15 works for example, does not complete for input

--- src/main/java/aoc2022/day15/BeaconExclusionZone.java
+++ src/main/java/aoc2022/day15/BeaconExclusionZone.java
@@ -14,8 +14,8 @@ import aoccommon.Point;
 public class BeaconExclusionZone {
 
   private static final String INPUT = "aoc2022/day15/input.txt";
-  // private static final int TARGET_ROW = 10;
-  private static final int TARGET_ROW = 2000000;
+  private static final int TARGET_ROW = 2000000; // 10;
+  private static final int MAX_XY = 4000000; // 20;
 
   private static final Pattern REGEX = Pattern
       .compile("^Sensor.*x=(\\-?\\d+),\\ y=(\\-?\\d+).*x=(\\-?\\d+),\\ y=(\\-?\\d+)$");
@@ -27,8 +27,11 @@ public class BeaconExclusionZone {
         .collect(Collectors.toMap(
             m -> Point.of(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2))),
             m -> Point.of(Integer.parseInt(m.group(3)), Integer.parseInt(m.group(4)))));
-    
+
     System.out.println("Part 1: " + coveredPointsInRow(sensorBeacons, TARGET_ROW).size());
+
+    Point beacon = findBeacon(sensorBeacons, MAX_XY);
+    System.out.println(beacon);
   }
 
   private static int manhattanDistance(Point a, Point b) {
@@ -49,5 +52,22 @@ public class BeaconExclusionZone {
       covered.remove(sensor.getValue());
     }
     return covered;
+  }
+
+  private static Point findBeacon(Map<Point, Point> sensorBeacons, int maxXY) {
+    for (int x = 0; x <= maxXY; x++) {
+      System.out.println(x);
+      pointLoop: for (int y = 0; y <= maxXY; y++) {
+        Point point = Point.of(x, y);
+        for (Map.Entry<Point, Point> sensor : sensorBeacons.entrySet()) {
+          if (manhattanDistance(sensor.getKey(), point) <= manhattanDistance(sensor.getKey(), sensor.getValue())) {
+            continue pointLoop;
+          }
+        }
+        // Point is outside coverage of all sensors.
+        return point;
+      }
+    }
+    return null;
   }
 }

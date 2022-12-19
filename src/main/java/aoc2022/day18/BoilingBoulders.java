b5ec1f2e76a392f9544237689d604e6596cd813d
mlandry <mike.landry@gmail.com>
2022-12-19 19:12:51 +0000

    Day 18 skeleton

---
+++ src/main/java/aoc2022/day18/BoilingBoulders.java
@@ -0,0 +1,10 @@
+package aoc2022.day18;
+
+/** Solution for {@link https://adventofcode.com/2022/day/18}. */
+public class BoilingBoulders {
+
+  private static final String INPUT = "aoc2022/day18/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

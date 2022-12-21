d2e176df8f2c7a5ee0a0629a5fbc071d703bce5d
mlandry <mike.landry@gmail.com>
2022-12-21 14:39:55 +0000

    Day 18 simplified to treat cubes as points, part 2 complete

--- src/main/java/aoc2022/day18/BoilingBoulders.java
+++ src/main/java/aoc2022/day18/BoilingBoulders.java
@@ -1,14 +1,11 @@
 package aoc2022.day18;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,162 +15,78 @@ import aoccommon.Point;
 /** Solution for {@link https://adventofcode.com/2022/day/18}. */
 public class BoilingBoulders {
 
-  private static final String INPUT = "aoc2022/day18/example4.txt";
+  private static final String INPUT = "aoc2022/day18/input.txt";
 
   public static void main(String[] args) throws Exception {
-    List<Cube> cubes = InputHelper.linesFromResource(INPUT).map(Cube::parse).collect(Collectors.toList());
+    Set<Point> cubes = InputHelper.linesFromResource(INPUT).map(Point::parse).collect(Collectors.toSet());
 
-    Map<Side, Integer> sideCounts = new HashMap<>();
-    for (Cube cube : cubes) {
-      for (Side side : cube.sides) {
-        sideCounts.compute(side, (k, v) -> v == null ? 1 : v + 1);
-      }
-    }
+    long externalSides = cubes.stream()
+        .map(BoilingBoulders::neighbours)
+        .mapToLong(s -> s.filter(n -> !cubes.contains(n)).count())
+        .sum();
 
-    Set<Side> externalSides = sideCounts.entrySet().stream()
-        .filter(e -> e.getValue() == 1)
-        .map(Map.Entry::getKey)
-        .collect(Collectors.toSet());
-    System.out.println("Part 1: " + externalSides.size());
+    System.out.println("Part 1: " + externalSides);
 
-    Set<Side> reachableExternalSides = findExternallyReachableSides(cubes, sideCounts, externalSides);
-    System.out.println("Part 2: " + reachableExternalSides.size());
+    long reachableSides = countExternallyReachableSides(cubes);
 
+    System.out.println("Part 2: " + reachableSides);
   }
 
-  private static Set<Side> findExternallyReachableSides(
-      List<Cube> cubes,
-      Map<Side, Integer> sideCounts,
-      Set<Side> externalSides) {
+  private static Stream<Point> neighbours(Point cube) {
+    return Stream.of(
+        Point.of(cube.getX() - 1, cube.getY(), cube.getZ()),
+        Point.of(cube.getX() + 1, cube.getY(), cube.getZ()),
+        Point.of(cube.getX(), cube.getY() - 1, cube.getZ()),
+        Point.of(cube.getX(), cube.getY() + 1, cube.getZ()),
+        Point.of(cube.getX(), cube.getY(), cube.getZ() - 1),
+        Point.of(cube.getX(), cube.getY(), cube.getZ() + 1));
+  }
 
-    Set<Point> externalSidePoints = externalSides.stream()
-        .map(Side::corners)
-        .flatMap(Set::stream)
-        .collect(Collectors.toSet());
-
-    // Build a bounding box to enclose the lava droplets
+  private static long countExternallyReachableSides(Set<Point> cubes) {
+    // Creating a bounding box larger than all the lava cubes.
     int[] xrange = new int[2];
     int[] yrange = new int[2];
     int[] zrange = new int[2];
-    for (Cube cube : cubes) {
-      xrange[0] = Math.min(cube.coordinates.getX() - 1, xrange[0]);
-      yrange[0] = Math.min(cube.coordinates.getY() - 1, yrange[0]);
-      zrange[0] = Math.min(cube.coordinates.getZ() - 1, zrange[0]);
+    for (Point cube : cubes) {
+      xrange[0] = Math.min(cube.getX() - 1, xrange[0]);
+      yrange[0] = Math.min(cube.getY() - 1, yrange[0]);
+      zrange[0] = Math.min(cube.getZ() - 1, zrange[0]);
 
-      xrange[1] = Math.max(cube.coordinates.getX() + 2, xrange[1]);
-      yrange[1] = Math.max(cube.coordinates.getY() + 2, yrange[1]);
-      zrange[1] = Math.max(cube.coordinates.getZ() + 2, zrange[1]);
+      xrange[1] = Math.max(cube.getX() + 1, xrange[1]);
+      yrange[1] = Math.max(cube.getY() + 1, yrange[1]);
+      zrange[1] = Math.max(cube.getZ() + 1, zrange[1]);
     }
 
-    // State for BFS traversal. The BFS finds all externally-reachable sides.
-    final Set<Point> visited = new HashSet<>();
-    final Queue<Point> queue = new LinkedList<>();
+    Point start = Point.of(xrange[0], yrange[0], zrange[0]);
 
-    Point current = Point.of(xrange[0], yrange[0], zrange[0]);
-    queue.offer(current);
-    visited.add(current);
+    Queue<Point> queue = new LinkedList<>();
+    queue.offer(start);
+
+    Set<Point> visited = new HashSet<>();
+    visited.add(start);
+
+    Map<Point, Integer> sideCounter = new HashMap<>();
 
     while (!queue.isEmpty()) {
-      current = queue.poll();
+      final Point current = queue.poll();
       neighbours(current)
           .filter(n -> !visited.contains(n))
-          .filter(n -> n.getX() >= xrange[0] && n.getX() <= xrange[1])
-          .filter(n -> n.getY() >= yrange[0] && n.getY() <= yrange[1])
-          .filter(n -> n.getZ() >= zrange[0] && n.getZ() <= zrange[1])
+          .filter(n -> inRange(n.getX(), xrange))
+          .filter(n -> inRange(n.getY(), yrange))
+          .filter(n -> inRange(n.getZ(), zrange))
           .forEach(n -> {
-            visited.add(n);
-            if (!externalSidePoints.contains(n)) {
-              queue.add(n);
+            if (cubes.contains(n)) {
+              sideCounter.compute(n, (k, v) -> v == null ? 1 : v + 1);
+            } else {
+              visited.add(n);
+              queue.offer(n);
             }
           });
     }
-    return externalSides.stream()
-        .filter(side -> side.corners().stream().anyMatch(visited::contains))
-        .collect(Collectors.toSet());
+    return sideCounter.values().stream().mapToInt(i -> i).sum();
   }
 
-  private static Stream<Point> neighbours(Point point) {
-    return Stream.of(
-        Point.of(point.getX() - 1, point.getY(), point.getZ()),
-        Point.of(point.getX() + 1, point.getY(), point.getZ()),
-        Point.of(point.getX(), point.getY() - 1, point.getZ()),
-        Point.of(point.getX(), point.getY() + 1, point.getZ()),
-        Point.of(point.getX(), point.getY(), point.getZ() - 1),
-        Point.of(point.getX(), point.getY(), point.getZ() + 1));
-  }
-
-  private static record Side(Set<Point> corners) {
-    static Side of(Point a, Point b, Point c, Point d) {
-      return new Side(Set.of(a, b, c, d));
-    }
-  }
-
-  private static class Cube {
-
-    private final Point coordinates;
-    private final Set<Side> sides;
-
-    private Cube(Point coordinates, Set<Side> sides) {
-      this.coordinates = coordinates;
-      this.sides = sides;
-    }
-
-    static Cube parse(String csv) {
-      Point coordinates = Point.parse(csv);
-      return new Cube(coordinates, sides(coordinates));
-    }
-
-    static Set<Side> sides(Point coordinates) {
-      Function<Integer, int[]> axisRange = i -> new int[] { i, i + 1 };
-      int[] xaxis = axisRange.apply(coordinates.getX());
-      int[] yaxis = axisRange.apply(coordinates.getY());
-      int[] zaxis = axisRange.apply(coordinates.getZ());
-
-      Set<Side> sides = new HashSet<>();
-      for (int xside = xaxis[0]; xside <= xaxis[1]; xside++) {
-        sides.add(Side.of(
-            Point.of(xside, yaxis[0], zaxis[0]),
-            Point.of(xside, yaxis[0], zaxis[1]),
-            Point.of(xside, yaxis[1], zaxis[0]),
-            Point.of(xside, yaxis[1], zaxis[1])));
-      }
-
-      for (int yside = yaxis[0]; yside <= yaxis[1]; yside++) {
-        sides.add(Side.of(
-            Point.of(xaxis[0], yside, zaxis[0]),
-            Point.of(xaxis[0], yside, zaxis[1]),
-            Point.of(xaxis[1], yside, zaxis[0]),
-            Point.of(xaxis[1], yside, zaxis[1])));
-      }
-
-      for (int zside = zaxis[0]; zside <= zaxis[1]; zside++) {
-        sides.add(Side.of(
-            Point.of(xaxis[0], yaxis[0], zside),
-            Point.of(xaxis[0], yaxis[1], zside),
-            Point.of(xaxis[1], yaxis[0], zside),
-            Point.of(xaxis[1], yaxis[1], zside)));
-      }
-
-      return sides;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-      if (!(other instanceof Cube)) {
-        return false;
-      }
-      Cube otherCube = (Cube) other;
-      return coordinates.equals(otherCube.coordinates);
-    }
-
-    @Override
-    public int hashCode() {
-      return coordinates.hashCode();
-    }
-
-    @Override
-    public String toString() {
-      return coordinates.toString();
-    }
+  private static boolean inRange(int p, int[] range) {
+    return p >= range[0] && p <= range[1];
   }
 }

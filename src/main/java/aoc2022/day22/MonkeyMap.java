f61aecedd0a387b5e37aedc7a7ee889ff801453f
mlandry <mike.landry@gmail.com>
2022-12-24 04:43:57 +0000

    Day 22 setup

---
+++ src/main/java/aoc2022/day22/MonkeyMap.java
@@ -0,0 +1,10 @@
+package aoc2022.day22;
+
+/** Solution for {@link https://adventofcode.com/2022/day/22}. */
+public class MonkeyMap {
+
+  private static final String INPUT = "aoc2022/day22/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

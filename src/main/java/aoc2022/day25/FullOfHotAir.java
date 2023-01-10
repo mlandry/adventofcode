dd9ad655a27d6e26839b2d48379f63f442150b22
mlandry <mike.landry@gmail.com>
2023-01-10 16:47:44 +0000

    Day 25 setup

---
+++ src/main/java/aoc2022/day25/FullOfHotAir.java
@@ -0,0 +1,10 @@
+package aoc2022.day25;
+
+/** Solution for {@link https://adventofcode.com/2022/day/25}. */
+public class FullOfHotAir {
+
+  private static final String INPUT = "aoc2022/day25/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

8f3e06dc22dc26d22b7b033e8acc4676e4d65ad8
mlandry <mike.landry@gmail.com>
2023-01-11 20:20:19 +0000

    2021 day 19 setup from scratch

---
+++ src/main/java/aoc2021/day19/BeaconScanner.java
@@ -0,0 +1,10 @@
+package aoc2021.day19;
+
+/** Solution for {@link https://adventofcode.com/2021/day/19}. */
+public class BeaconScanner {
+
+  private static final String INPUT = "aoc2021/day19/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}

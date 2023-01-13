d9d9f34f540e707d19efe823d6ac6f9e64683c62
mlandry <mike.landry@gmail.com>
2023-01-13 18:57:32 +0000

    2021 day 20 setup

---
+++ src/main/java/aoc2021/day20/TrenchMap.java
@@ -0,0 +1,10 @@
+package aoc2021.day20;
+
+/** Solution for {@link https://adventofcode.com/2021/day/20}. */
+public class TrenchMap {
+
+  private static final String INPUT = "aoc2021/day20/input.txt";
+
+  public static void main(String [] args) throws Exception {
+  }
+}
